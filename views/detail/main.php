f2b1ba9d5e3971d2df00192c53b8d2ef6df0bb2b
凤之兮原 <1577310596@qq.com>
2023-05-09 14:07:55 +0800

    chore: add some card for detail

--- views/detail/main.php
+++ views/detail/main.php
@@ -1,3 +1,30 @@
-<?php
-include_once '../components/title.php';
-?>
+<main class="flex justify-around">
+
+  <aside>
+    <?php
+    include_once '../components/title.php';
+    ?>
+  </aside>
+
+  <aside class='flex flex-col'>
+    <div class="top">
+      <?php
+      include_once '../components/user_card.php';
+      ?>
+    </div>
+    <div class="bottom">
+      <div class="card w-96 bg-neutral text-neutral-content">
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">Cookies!</h2>
+          <p>We are using cookies for no reason.</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Accept</button>
+            <button class="btn btn-ghost">Deny</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </aside>
+
+
+</main>
