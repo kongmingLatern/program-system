579e1774fb03df8e83a29b9870826b60c0794ff1
凤之兮原 <1577310596@qq.com>
2023-05-09 22:06:15 +0800

    feat: add dropmenu to avatar

--- views/index/header.php
+++ views/index/header.php
@@ -1,9 +1,13 @@
 <header class="navbar flex justify-between border-b-[2px]">
   <h1 class="font-bold text-2xl px-4">新闻发布系统</h1>
 
-  <div class="avatar placeholder cursor-pointer" onclick="redirectToPersonal()">
+  <div class="avatar dropdown dropdown-left dropdown-hover placeholder cursor-pointer" onclick="redirectToPersonal()">
     <div class="bg-neutral-focus text-neutral-content rounded-full w-12">
       <span class="text-sm">友利</span>
     </div>
+    <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+      <li><a>个人中心</a></li>
+      <li><a>写文章</a></li>
+    </ul>
   </div>
 </header>
