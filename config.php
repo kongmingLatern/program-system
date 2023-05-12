3c6c99b38c5c5f03c563cf4eb6d1a97b55918782
凤之兮原 <1577310596@qq.com>
2023-05-12 17:16:09 +0800

    chore: change config.php

--- config.php
+++ config.php
@@ -1,7 +1,11 @@
 <?php
-$host = "10.18.57.16";
-$user = "H_092220120";
-$password = "Syj011023";
+// $host = "10.18.57.16";
+// $user = "H_092220120";
+// $password = "Syj011023";
+// $database = "H_092220120";
+$host = "8.130.66.162";
+$user = "root";
+$password = "root";
 $database = "H_092220120";
 
 $conn = new mysqli($host, $user, $password, $database);
